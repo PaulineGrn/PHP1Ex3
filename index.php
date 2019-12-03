93bec873989609aa1ba5bc051ac7bd5651db1d7e
PaulineGrn <pauline.gernigon@orange.fr>
2019-12-03 09:19:40 +0100

    PHP1Ex4

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+$km=1;
+?>
+<!DOCTYPE html>
+<html lang=fr dir="ltr">
+<head>
+  <meta charset="utf-8"/>
+  <title>PHP1Ex3</title>
+</head>
+<body>
+  <p>
+    <?=$km;?>
+  </p>
+  <p>
+    <?=$km+2;?>
+  </p>
+  <p>
+    <?=125;?>
+  </p>
+</body>
+</html>
